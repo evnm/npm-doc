f662dbaba5d7b50ca43ca70765b0d6d030da00b4
Evan Meagher <evan.meagher@gmail.com>
2011-01-05 17:26:37 -0800

    Remove npm load interleaving via setTimeout thanks to npm fix (d61c3b4).

--- test/npm-doc.test.js
+++ test/npm-doc.test.js
@@ -1,21 +1,17 @@
 var assert = require('assert')
   , doc = require('../lib/npm-doc');
 
-// So hacky...git multiple calls to getDocs can't be made concurrently, so I'm
-// using arbitrary setTimeouts to try to space things out.
 module.exports = {
   'test failure on invalid pkg': function () {
-    doc.getDocs('not-a-valid-pkg-asdf', {npm_loglevel: 'silent'},
-                function (err, data) {
-                  assert.isNotNull(err);
-              });
+    doc.getDocs('not-a-valid-pkg-asdf', function (err, data) {
+      assert.isNotNull(err);
+    });
   },
 
   'test failure on invalid pkg with version': function () {
-    setTimeout(function () {
-      doc.getDocs({'not-a-valid-pkg-asdf': '0.0.1'}, function (err, data) {
-        assert.isNotNull(err);
-      })}, 500);
+    doc.getDocs({'not-a-valid-pkg-asdf': '0.0.1'}, function (err, data) {
+      assert.isNotNull(err);
+    })
   },
 
   'test failure on invalid version of valid pkg': function () {
@@ -31,26 +27,20 @@ module.exports = {
   },
 
   'test success on pkgname string': function () {
-    setTimeout(function () {
-      doc.getDocs('npm', function (err, data) {
-        assert.isDefined(data);
-      });
-    }, 1000);
+    doc.getDocs('npm', function (err, data) {
+      assert.isDefined(data);
+    });
   },
 
   'test success on pkgname array': function () {
-    setTimeout(function () {
-      doc.getDocs(['npm'], function (err, data) {
-        assert.isDefined(data);
-      });
-    }, 1500);
+    doc.getDocs(['npm'], function (err, data) {
+      assert.isDefined(data);
+    });
   },
 
   'test success on pkgname:version object': function () {
-    setTimeout(function () {
-      doc.getDocs({'dropbox': '0.1.3'}, function (err, data) {
-        assert.isDefined(data);
-      });
-    }, 2000);
+    doc.getDocs({'dropbox': '0.1.3'}, function (err, data) {
+      assert.isDefined(data);
+    });
   }
 }
